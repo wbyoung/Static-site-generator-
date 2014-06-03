2760076026cc651087184d53aa392f298d6ed682
Whitney Young <wbyoung@fadingred.com>
2014-06-03 13:04:39 -0700

    Renamed underscore.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 var chai = require('chai');
 var expect = chai.expect;
-var _ = require('../index');
+var lib = require('../index');
 var path = require('path');
 
 describe('fileReader()', function() {
 	it('should read a file', function(done) {
-		_.fileReader('text.txt', function (err, contents) {
+		lib.fileReader('text.txt', function (err, contents) {
 			expect(contents).to.be.eql('this is just some text');
 			done();
 		});
@@ -14,13 +14,13 @@ describe('fileReader()', function() {
 
 describe('pathReader()', function() {
 	it('shoud turn directory items into a number of arrays', function(done) {
-		_.pathReader('source', function(err, files) {
+		lib.pathReader('source', function(err, files) {
 			expect(files.length).to.be.eql(3);
 			done();
 		});
 	});
 	it('should return the array', function(done) {
-		_.pathReader('test', function(err, files) {
+		lib.pathReader('test', function(err, files) {
 			expect(files).to.be.eql(['.DS_Store', 'test.js', 'text.txt']);
 			done();
 		});
@@ -29,16 +29,16 @@ describe('pathReader()', function() {
 
 describe('isHtml()', function() {
 	it('should return false for non-html files', function () {
-		expect(_.isHtml('blablab.hml')).to.be.eql(false);
+		expect(lib.isHtml('blablab.hml')).to.be.eql(false);
 	});
 	it('should return true for html files', function() {
-		expect(_.isHtml('blah.html')).to.be.eql(true);
+		expect(lib.isHtml('blah.html')).to.be.eql(true);
 	});
 });
 
 describe('fileShreader()', function() {
 	it.skip('deletes files inside a direcotry', function(done) {
-		_.fileShreader(function(err, files) {
+		lib.fileShreader(function(err, files) {
 			expect();
 		});
 	});
