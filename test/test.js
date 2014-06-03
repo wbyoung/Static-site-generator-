65ccebb7d9c719ec63fdb89e12dd42a5983dd57d
Tian Song <tian.y.song@gmail.com>
2014-06-03 10:54:06 -0700

    function for read file path and split default template

--- test/test.js
+++ test/test.js
@@ -1,9 +1,31 @@
 var chai = require('chai');
 var expect = chai.expect;
 var _ = require('../sitegen');
+var path = require('path');
 
-describe('insertfunction()', function() {
-	it('should test true', function() {
-		expect(_.fileSaver()).to.be.eql(true);
+describe('fileReader()', function() {
+		console.log(__dirname);
+	var fixture = path.join(__dirname, 'text.txt');
+
+	it('should read a file', function(done) {
+		_.fileReader(fixture, function (err, contents) {
+			expect(contents).to.be.eql('this is just some text');
+			done();
+		});
+	});
+});
+
+describe('pathReader()', function() {
+	it('shoud turn directory items into an array', function(done) {
+		_.pathReader('source', function(err, files) {
+			expect(files.length).to.be.eql(3);
+			done();
+		});
+	});
+});
+
+describe('isHtml()', function() {
+	it('should return true for html files', function () {
+		expect(_.isHtml('blablab.hml')).to.be.eql(false);
 	});
 });
